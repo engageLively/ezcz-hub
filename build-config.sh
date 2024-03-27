6eaf5a56dec9eada504fbe5f40c9f414118a4834
Rick McGeer <rick@mcgeer.com>
2024-03-27 10:39:10 -0700

    Updated to use jwt

--- build-config.sh
+++ build-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cat proxy.yaml > config.yaml
 echo "" >> config.yaml
-echo 'hub:' >> config.yaml
+cat hub.yaml >> config.yaml
 echo '  extraConfig:' >> config.yaml
 cat localConfig.yaml >> config.yaml
 cat body.yaml >> config.yaml
